ea2caf665d0e6abfbd054e27efca775e6e32bb07
DAW2 <DAW2@PC25>
2025-03-04 17:07:59 +0100

    Añadir clase login

---
+++ Calculadora/src/main/java/com/mycompany/calculadora/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author DAW2
+ */
+public class Login {
+    // Añade un método para simular un login
+
+    public boolean autenticar(String usuario, String contraseña) {
+
+        // Implementa la lógica de autenticación
+        return false;
+        
+    }
+}
